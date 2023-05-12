0a569e0b1b529a4ff3b60f7cd75db87cb34f153b
user404e <e404user@gmail.com>
2023-05-12 11:00:42 +0000

    PYTHON

---
+++ GENERATE RANDOM INTEGER NUMBER/RandomNumber.py
@@ -0,0 +1,8 @@
+from random import randint
+
+print("Enter Range - ")
+
+start = int(input("Enter Starting Number : "))
+end = int(input("Enter Ending Number : "))
+
+print(randint(start,end))
---
+++ SQUARE OF LIST ELEMENTS/Square.py
@@ -0,0 +1,4 @@
+lst = [1,2,3,4,5]
+
+for i in lst:
+    print(i*i)
