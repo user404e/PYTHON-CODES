6325cca206b8c1faae30f5518d38833a36dd8e38
user404e <e404user@gmail.com>
2023-05-30 19:26:57 +0000

    PYTHON

---
+++ GENERATE RANDOM NUMBER FROM GIVEN RANGE/GenerateRandomNumber.py
@@ -0,0 +1,8 @@
+from random import randint
+
+start = int(input("Enter Starting Point Of Range : "))
+end = int(input("Enter Ending Point Of Range : "))
+
+rand_number = randint(start,end)
+
+print(rand_number)
